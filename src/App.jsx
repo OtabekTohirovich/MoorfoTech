f4de112d2b6324c2b0661100f0421f1bdd1315c4
OtabekTohirovich <tohirovo630@gmail.com>
2023-08-03 18:24:49 +0500

    Redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import { useTranslation } from "react-i18next";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
@@ -15,10 +15,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/services" element={<ServicesPage />} />
-        <Route
-          path="*"
-          element={<Navigate to="/" replace />}
-        />
       </Routes>
       <Footer />
     </>
